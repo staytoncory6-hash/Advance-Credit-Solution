38da500b54ecfbea5a013e7170fd7ac1e5e9a4b9
staytoncory6-hash <staytoncory6@gmail.com>
2025-11-23 22:05:36 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCeB-_qx3SD9ABfbq0ZHI3A9cUNtjQhVZ0",
+    authDomain: "elite-credit.firebaseapp.com",
+    projectId: "elite-credit",
+    storageBucket: "elite-credit.firebasestorage.app",
+    messagingSenderId: "1010475492974",
+    appId: "1:1010475492974:web:032c9b4a7a97c177dfe87f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
